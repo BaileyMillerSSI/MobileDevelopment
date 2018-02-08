06504c6871b4f50664f295b726ef90ddd82cf353
Bailey Miller <bmiller@susquehannasoftware.com>
2018-02-08 12:15:20 -0500

    Add manual rotation

--- Android Studio/Buttons/app/src/main/java/com/school/penncollege/buttons/MainActivity.java
+++ Android Studio/Buttons/app/src/main/java/com/school/penncollege/buttons/MainActivity.java
@@ -1,8 +1,10 @@
 package com.school.penncollege.buttons;
 
+import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,5 +31,10 @@ public class MainActivity extends AppCompatActivity {
         {
             setContentView(R.layout.activity_other);
         }
+    }
+
+    public void RotateScreen(View view)
+    {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
