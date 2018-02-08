fea39deacaffe0e1c4e60664e0bbadbe3d03f646
Bailey Miller <bmiller@susquehannasoftware.com>
2018-02-08 11:55:31 -0500

    Initial?

---
+++ Android Studio/Buttons/app/src/main/java/com/school/penncollege/buttons/MainActivity.java
@@ -0,0 +1,34 @@
+package com.school.penncollege.buttons;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Configuration config = getResources().getConfiguration();
+        modifyLayout(config);
+    }
+
+    public void onConfigurationChanged(Configuration config)
+    {
+        super.onConfigurationChanged(config);
+        modifyLayout(config);
+    }
+
+    public void modifyLayout(Configuration config)
+    {
+        if(config.orientation == Configuration.ORIENTATION_LANDSCAPE)
+        {
+            setContentView(R.layout.activity_main);
+        }else if(config.orientation == Configuration.ORIENTATION_PORTRAIT)
+        {
+            setContentView(R.layout.activity_other);
+        }
+    }
+}
