8efaf387eeb3ff328380b00cb279b0c358c72d52
Bailey Miller <bmiller@susquehannasoftware.com>
2018-02-15 11:41:33 -0500

    Fleshing out the todo item class.

--- Android Studio/TodoList/app/src/main/java/com/school/penncollege/todolist/TodoItem.java
+++ Android Studio/TodoList/app/src/main/java/com/school/penncollege/todolist/TodoItem.java
@@ -1,8 +1,45 @@
 package com.school.penncollege.todolist;
 
+import java.util.Date;
+
 /**
  * Created by Bailey Miller on 2/15/2018.
  */
 
-public class TodoItem {
+public class TodoItem
+{
+    public int ItemId;
+    public boolean IsDone;
+    public String Title;
+    public Date DateCreated;
+
+
+
+    public TodoItem(String t)
+    {
+        this.Title = t;
+        this.DateCreated = new Date();
+        this.IsDone = false;
+    }
+    
+    public boolean GetStatus()
+    {
+        return this.IsDone;
+    }
+
+    public void SetStatus(boolean status)
+    {
+        this.IsDone = status;
+    }
+
+    public String GetTitle()
+    {
+        return this.Title;
+    }
+
+    public Date GetCreationTime()
+    {
+        return this.DateCreated;
+    }
+
 }
