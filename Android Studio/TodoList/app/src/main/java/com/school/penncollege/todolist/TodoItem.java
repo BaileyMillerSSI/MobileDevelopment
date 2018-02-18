76c51bc021a19a85568cef8a49cfb8fab81f63b0
Bailey Miller <bmiller@susquehannasoftware.com>
2018-02-18 15:44:10 -0500

    Lots of improvements to make this class work

--- Android Studio/TodoList/app/src/main/java/com/school/penncollege/todolist/TodoItem.java
+++ Android Studio/TodoList/app/src/main/java/com/school/penncollege/todolist/TodoItem.java
@@ -1,6 +1,7 @@
 package com.school.penncollege.todolist;
 
 import java.util.Date;
+import java.text.SimpleDateFormat;
 
 /**
  * Created by Bailey Miller on 2/15/2018.
@@ -21,44 +22,99 @@ public class TodoItem
         this.GetData();
     }
 
+    public TodoItem(String title, Date dc, Date dd, boolean status)
+    {
+        // Set these values
+        this.Title = title;
+        this.DateCreated = dc;
+        this.DateDue = dd;
+        this.IsDone = status;
+        //Call the create function
+        MainActivity.dbManager.insert(this);
+    }
+
     public int GetId()
     {
         return  this.ItemId;
     }
-
-    public boolean GetStatus()
+    public int GetStatus()
     {
-        return this.IsDone;
+        return (this.IsDone) ? 1: 0;
     }
-
     public void SetStatus(boolean status)
     {
         this.IsDone = status;
         this.UpdateDatabase();
     }
 
-    public String GetTitle()
+    public void ToggleStatus()
     {
-        return this.Title;
+        SetStatus(!IsDone);
     }
 
-    public Date GetCreationTime()
+    public String GetTitle(boolean wrap)
     {
-        return this.DateCreated;
+        if(wrap)
+        {
+            return "\"" + this.Title + "\"";
+        }else
+        {
+            return this.Title;
+        }
+    }
+    public void SetTitle(String t)
+    {
+        this.Title = t;
+        this.UpdateDatabase();
     }
 
+    public String GetCreationTime(boolean wrap)
+    {
+        if(wrap)
+        {
+            SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+            return "\"" + formatter.format(this.DateCreated) + "\"";
+        }else
+        {
+            SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+            return formatter.format(this.DateCreated);
+        }
+    }
 
+    public String GetDueTime(boolean wrap)
+    {
+        if(wrap)
+        {
+            SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+            return "\"" + formatter.format(this.DateDue) + "\"";
+        }else
+        {
+            SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+            return formatter.format(this.DateDue);
+        }
+    }
+    public void SetDueTime(Date d)
+    {
+        this.DateDue =d;
+        this.UpdateDatabase();
+    }
+
+    public void SetValues(String title, Date dc, Date dd, boolean status)
+    {
+        this.Title = title;
+        this.DateCreated = dc;
+        this.DateDue = dd;
+        this.IsDone = status;
+    }
 
     // Have the model tell the database an update has occurred?
     private void UpdateDatabase()
     {
-        // Call a static method that updates the database according to this model
-        // Database.UpdateModel(this);
+        MainActivity.dbManager.update(this);
     }
 
     private void GetData()
     {
-        // Call a static method that gets all the fields from the database for this model
-        // Database.GetModel(this);
+        MainActivity.dbManager.select(this);
     }
 }
