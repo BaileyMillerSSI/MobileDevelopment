d9007d92fbc3b03b23fa1e0c134a7217797ff7a8
jonhen11 <jonhen11@hotmail.com>
2018-02-18 21:43:03 -0500

    getting lost in the code

--- Android Studio/TodoList/app/src/main/java/com/school/penncollege/todolist/InsertActivity.java
+++ Android Studio/TodoList/app/src/main/java/com/school/penncollege/todolist/InsertActivity.java
@@ -19,17 +19,19 @@ public class InsertActivity extends AppCompatActivity {
     }
 
     public void insert( View v ) {
-        // Retrieve name and price
+        // Retrieve task and due date
         EditText taskEditText = ( EditText) findViewById( R.id.input_task );
         EditText dateEditText = ( EditText) findViewById( R.id.input_date );
-        String task = taskEditText.getText( ).toString( );
-        String dateString = dateEditText.getText( ).toString( );
+        String taskString = taskEditText.getText( ).toString( );
+        String dateDueString = dateEditText.getText( ).toString();
+        Boolean status = False;
+        String dateCurString = "Current Date";
 
         // insert new task in database
         try {
             String task = String.copyValueOf();
-            Task candy = new Candy( 0, name, price );
-            dbManager.insert(name);
+            TodoItem newTask = new TodoItem(taskString,dateDueString,dateCurString, status);
+            dbManager.insert(newTask);
             Toast.makeText( this, "Task added", Toast.LENGTH_SHORT ).show( );
         } catch( NumberFormatException nfe ) {
             Toast.makeText( this, "error", Toast.LENGTH_LONG ).show( );
