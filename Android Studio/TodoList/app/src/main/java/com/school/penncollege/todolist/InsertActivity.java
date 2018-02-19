eda41b5a8acede0d72e778002277f7845308d2b7
Bailey Miller <bmiller@susquehannasoftware.com>
2018-02-18 20:12:18 -0500

    Corrected code.

--- Android Studio/TodoList/app/src/main/java/com/school/penncollege/todolist/InsertActivity.java
+++ Android Studio/TodoList/app/src/main/java/com/school/penncollege/todolist/InsertActivity.java
@@ -9,16 +9,33 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class InsertActivity extends AppCompatActivity {
+import java.util.Date;
 
+public class InsertActivity extends AppCompatActivity {
+    private DatabaseManager dbManager;
 
     public void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
-
+        dbManager = new DatabaseManager( this );
         setContentView( R.layout.activity_insert );
     }
 
     public void insert( View v ) {
+        // Get all the values
+        // Get Title
+        // Get DateDue
+
+        //v.findViewById(R.id.input_title);
+        //v.findViewById(R.id.input_Date_Due);
+
+        // Create using the model
+        // This will create a new todo item and store it in the database
+        new TodoItem("", new Date(), new Date(), false);
+
+        // Reset the view
+        // Reset Title
+
+        // Reset Due Date
 
     }
 
