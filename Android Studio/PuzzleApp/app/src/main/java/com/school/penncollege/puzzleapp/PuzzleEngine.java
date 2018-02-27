56b44b4586e2800202ff998793987204197c0512
Bailey Miller <bmiller@susquehannasoftware.com>
2018-02-27 11:32:16 -0500

    Added basic setup for the game

--- Android Studio/PuzzleApp/app/src/main/java/com/school/penncollege/puzzleapp/PuzzleEngine.java
+++ Android Studio/PuzzleApp/app/src/main/java/com/school/penncollege/puzzleapp/PuzzleEngine.java
@@ -2,5 +2,38 @@ package com.school.penncollege.puzzleapp;
 
 public class PuzzleEngine
 {
+    // Used for starting games
+    public static final int EasyMode = 9; // 3x3
+    public static final int MediumMode = 12; //3x4
+    public static final int HardMode = 16; //4x4
 
+    // Means the game hasn't started
+    private int CurrentGameMode = 0;
+
+    private int[][] Board;
+
+    public int GetCurrentGameSize()
+    {
+        return this.CurrentGameMode;
+    }
+
+    public PuzzleEngine(int size)
+    {
+        this.CurrentGameMode = size;
+
+        // Determine game mode
+        switch(size)
+        {
+            case PuzzleEngine.EasyMode:
+                this.Board = new int[3][3];
+                break;
+            case PuzzleEngine.MediumMode:
+                this.Board = new int[3][4];
+                break;
+
+            case PuzzleEngine.HardMode:
+                this.Board = new int[4][4];
+                break;
+        }
+    }
 }
