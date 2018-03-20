d39fb41bf1f5fb1a99cac51f62f50954f44b0838
Bailey Miller <bmiller@susquehannasoftware.com>
2018-03-20 11:43:41 -0400

    Messing around

--- Android Studio/MapWithMarker/app/src/main/java/MapHelpers.java
+++ Android Studio/MapWithMarker/app/src/main/java/MapHelpers.java
@@ -1,3 +1,8 @@
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+
 import com.google.android.gms.location.FusedLocationProviderApi;
 import com.google.android.gms.location.LocationServices;
 
@@ -5,16 +10,24 @@ import com.google.android.gms.location.LocationServices;
  * Created by Bailey Miller on 3/20/2018.
  */
 
-public class MapHelpers
-{
+public class MapHelpers {
 
 
-
-
-    public void GetUserLocation()
+    public void GetUserLocation(Context context)
     {
         FusedLocationProviderApi locationApi = LocationServices.FusedLocationApi;
-        location = locationApi.getLastLocation();
+
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        
     }
 
 }
