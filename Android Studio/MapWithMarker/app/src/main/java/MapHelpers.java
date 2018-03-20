7b9d1c205932e5e02c5315b839c71f1322cda426
Bailey Miller <bmiller@susquehannasoftware.com>
2018-03-20 11:27:43 -0400

    Broken code

--- Android Studio/MapWithMarker/app/src/main/java/MapHelpers.java
+++ Android Studio/MapWithMarker/app/src/main/java/MapHelpers.java
@@ -1,8 +1,20 @@
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationServices;
+
 /**
  * Created by Bailey Miller on 3/20/2018.
  */
 
 public class MapHelpers
 {
+
+
+
+
+    public void GetUserLocation()
+    {
+        FusedLocationProviderApi locationApi = LocationServices.FusedLocationApi;
+        location = locationApi.getLastLocation();
+    }
 
 }
